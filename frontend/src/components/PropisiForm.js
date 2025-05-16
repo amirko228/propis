81159e60728adeda09cead672588f04af65ea160
amir <ogonbashevamir@gmail.com>
2025-05-16 19:21:56 +0600

    Исправил

--- frontend/src/components/PropisiForm.js
+++ frontend/src/components/PropisiForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 // Библиотека успешно установлена
 import { Document, Page, pdfjs } from 'react-pdf';
